206bf7e6c29448fd66ee0c28f988a56bb5583089
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-03-30 20:22:07 -0700

    commit

--- models/models.js
+++ models/models.js
@@ -1,8 +1,8 @@
-const { Model, DataTypes, UUIDV4 } = require("sequelize")
-const sequelize = require("../config/connection")
-const mailer = require("../email/mailer")
-const bcrypt = require("bcrypt")
-const salt = 10
+const { Model, DataTypes, UUIDV4 } = require('sequelize');
+const sequelize = require('../config/connection');
+const mailer = require('../email/mailer');
+const bcrypt = require('bcrypt');
+const salt = 10;
 
 class User extends Model {
   checkPassword(pw) {
@@ -22,87 +22,91 @@ const validate = {
 User.init(
   {
     id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING,
-        validate: {
-            len: [2, 100],
-            is: validate.name,
-        },
+      type: DataTypes.STRING,
+      validate: {
+        len: [2, 100],
+        is: validate.name,
+      },
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            is: validate.username,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        // is: validate.username,
+      },
     },
     email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-            isEmail: true,
-        },
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [0, 60],
-            is: validate.password
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 60],
+        // is: validate.password,
+      },
     },
     emailCode: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: true
-    }
-}, {
-    sequelize, 
-    modelName: 'user', 
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'user',
     hooks: {
-        async beforeCreate(newUserData) {
-            newUserData.password = await bcrypt.hash(newUserData.password, salt)
-            return newUserData
-        },
-        async afterCreate(newUserData) {
-            const id = newUserData.emailCode
-            mailer.verificationEmail(id, newUserData.email)
-            return newUserData
-        }
-    }
-})
-Project.init({
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, salt);
+        return newUserData;
+      },
+      async afterCreate(newUserData) {
+        const id = newUserData.emailCode;
+        mailer.verificationEmail(id, newUserData.email);
+        return newUserData;
+      },
+    },
+  }
+);
+Project.init(
+  {
     id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          is: validate.name,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: validate.name,
+      },
     },
     creator: {
-        type: DataTypes.UUID,
-        allowNull: false,
+      type: DataTypes.UUID,
+      allowNull: false,
     },
     description: {
-        type: DataTypes.STRING(5000)
+      type: DataTypes.STRING(5000),
     },
     endpoint: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-    }
-}, { 
-    sequelize, 
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
     modelName: 'project',
   }
 );

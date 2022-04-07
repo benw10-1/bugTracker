22a66ef9ff1c7f772acabefeae7adef77037d588
Ben Wirth <benjaminwirth10@gmail.com>
2022-04-06 21:07:28 -0700

    new fix

--- routes/routes.js
+++ routes/routes.js
@@ -101,7 +101,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
   }
 });
 
-router.get('/projects/:id/bugs', withAuth, async (req, res) => {
+router.get('/projects/:id', withAuth, async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id);
 
@@ -133,5 +133,16 @@ router.get('/projects/:id/bugs', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get('/projects/:id/submitBug', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id);
+    if (projectData.endpoint) {
+      res.render("bugform")
+    }
+    else throw "Error"
+  } catch (err) {
+    res.redirect("/")
+  }
+})
 
 module.exports = router;
